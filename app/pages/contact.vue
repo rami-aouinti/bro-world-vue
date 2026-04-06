80cb5074fd30cc1c2291e32883748b5cab08f2d0
Rami Aouinti <rami.aouinti@gmail.com>
2026-04-07 01:05:16 +0200

    add css

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -50,7 +50,7 @@ definePageMeta({
       </template>
     </PageDrawers>
 
-    <v-container class="py-12 text-center">
+    <v-container fluid class="text-center">
       <h1 class="text-h3 mb-4">Contact</h1>
       <p>Contactez-nous pour toute question ou demande de collaboration.</p>
     </v-container>
