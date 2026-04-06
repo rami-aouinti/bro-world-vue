0f394277c385ba33aeac818bafb49d3721b122a3
Rami <rami.aouinti@gmail.com>
2026-04-06 22:35:42 +0200

    Make left and right drawer list areas match

--- app/components/App/AppRightDrawer.vue
+++ app/components/App/AppRightDrawer.vue
@@ -11,15 +11,26 @@ const rightDrawerOpen = useState('right-drawer-open', () => true)
     floating
     class="app-right-drawer"
   >
-    <div class="pa-4" />
+    <v-list nav density="compact" class="app-right-drawer-list" />
+    <v-spacer />
+    <template #append>
+      <v-list-item class="drawer-footer px-0" />
+    </template>
   </v-navigation-drawer>
 </template>
 
 <style scoped>
 .app-right-drawer {
   border-radius: 24px;
-  margin-top: 82px;
+  margin-top: 56px;
   margin-right: 16px;
-  height: calc(100% - 98px);
+  height: calc(100% - 72px);
+}
+
+.app-right-drawer-list {
+  height: calc(100% - 96px);
+  min-height: calc(100% - 96px);
+  max-height: calc(100% - 96px);
+  overflow-y: auto;
 }
 </style>
