9c778f40930387f40fe766677c0262b2c43f11a4
Rami <rami.aouinti@gmail.com>
2026-04-07 18:06:00 +0200

    feat(blog): add stories proxy and carousel mapping

---
+++ server/api/stories/index.get.ts
@@ -0,0 +1,5 @@
+import { callPrivateApi } from '../../utils/privateApi'
+
+export default defineEventHandler(async (event): Promise<unknown> => {
+  return callPrivateApi(event, '/api/v1/private/stories')
+})
