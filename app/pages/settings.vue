15b7ab4e172915210e06abc6d4484b2521d16af1
Rami <rami.aouinti@gmail.com>
2026-04-07 02:58:12 +0200

    Add profile and settings entries to user menu

---
+++ app/pages/settings.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const { t } = useI18n()
+
+definePageMeta({
+  title: 'appbar.settings',
+})
+</script>
+
+<template>
+  <v-container class="py-12">
+    <h1 class="text-h3 mb-4">{{ t('appbar.settings') }}</h1>
+    <p>Personnalisez vos préférences et les paramètres de votre compte.</p>
+  </v-container>
+</template>
