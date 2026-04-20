0841c406bc13b6db65f2935d1c9c460f9702d57d
Rami <rami.aouinti@gmail.com>
2026-04-20 05:27:17 +0200

    fix(crm): make latest sprint tasks endpoint public

--- server/api/crm/general/tasks/by-latest-sprint/index.get.ts
+++ server/api/crm/general/tasks/by-latest-sprint/index.get.ts
@@ -1,5 +1,4 @@
 import type { CrmTaskItem } from '~~/server/types/api/crm-general'
-import { getSessionAuth } from '~~/server/utils/privateApi'
 import { cachedCrmGeneralGet } from '~~/server/utils/crmGeneralPrivateApi'
 
 interface CrmSprintBucket {
@@ -21,7 +20,6 @@ interface CrmTasksByLatestSprintResponse {
 }
 
 export default defineEventHandler(async (event): Promise<CrmTasksByLatestSprintResponse> => {
-  await getSessionAuth(event)
 
   return cachedCrmGeneralGet<CrmTasksByLatestSprintResponse>(
     event,
